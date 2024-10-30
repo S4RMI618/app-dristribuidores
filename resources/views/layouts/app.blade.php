40740aef585599e2e9ff4dfeff513c776daa69c6
S4RMI618 <huessabe@gmail.com>
2024-10-30 15:38:51 -0500

    resources-path-edited

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,13 +13,15 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
+    <!-- Scripts -->
     @if (app()->environment('local'))
         <!-- Development -->
         <script src="https://cdn.tailwindcss.com"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
         <!-- Production -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
+        <link rel="stylesheet" href="{{ asset('build/assets/app-CH4tXQnY.css') }}">
+        <script src="{{ asset('build/assets/app-BEhEqZ9O.js') }}" defer></script>
     @endif
 </head>
 
